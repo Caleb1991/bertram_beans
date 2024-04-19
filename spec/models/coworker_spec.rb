9041690389f8ab86ad9be75a119b69b5c19d5c22
Caleb Wittman <CalebJWittman1991@Gmail.Com>
2024-04-18 19:35:40 -0600

    add cost_burden_incurred validation to Coworker model

--- spec/models/coworker_spec.rb
+++ spec/models/coworker_spec.rb
@@ -4,5 +4,6 @@ RSpec.describe Coworker, type: :model do
   describe 'validations' do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name) }
+    it { should validate_numericality_of(:cost_burden_incurred) }
   end
 end
