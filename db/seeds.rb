940b62a8ff464f8fe92ff5562b34c27a4e205458
Caleb Wittman <CalebJWittman1991@Gmail.Com>
2024-04-21 21:55:17 -0600

    update seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Coworker.destroy_all
+Beverage.destroy_all
+CoworkerBeverage.destroy_all
+
+beverage_cost = 2.00
+
+['Cappuccino', 'Americano', 'Tea, Earl Grey, Hot', 'Cappucino', 'Green Tea', 'Cold Brew', 'Mocha'].each do |beverage_name|
+  Beverage.create!(name: beverage_name, cost: beverage_cost)
+  beverage_cost += 2.00
+end
+
+['Jeremy', 'Bob', 'Anya', 'Bondo', 'Loid', 'Yor', 'Becky'].each do |coworker_name|
+  Coworker.create!(name: coworker_name)
+end
+
+7.times do |i|
+  CoworkerBeverage.create!(beverage_id: Beverage.all[i].id, coworker_id: Coworker.all[i].id)
+end
